457e4468b59e2d115a8c14a3515a551d79da0c0e
gleison araujo <araujo.gleison67@gmail.com>
2024-11-11 23:48:22 -0300

    feat: created api hospede

---
+++ server/src/main/java/com/example/server/api/hospede/HospedeController.java
@@ -0,0 +1,33 @@
+package com.example.server.api.hospede;
+
+import java.util.List; // Importa a classe List para trabalhar com listas de objetos
+import java.util.concurrent.CompletableFuture;  // Importa a classe CompletableFuture para trabalhar com funções assíncronas
+import java.util.concurrent.ExecutionException; // Importa a classe ExecutionException para lidar com exceções ao esperar o resultado de tarefas assíncronas
+
+import org.springframework.beans.factory.annotation.Autowired; // Importa o Autowired para permitir a injeção de dependências no Spring
+import org.springframework.http.HttpStatus; // Importa a classe HttpStatus para trabalhar com códigos de status HTTP
+import org.springframework.http.ResponseEntity; // Importa a classe ResponseEntity para criar respostas HTTP personalizadas
+
+import org.springframework.web.bind.annotation.*; // Importa todas as anotações relacionadas a controladores REST no Spring MVC (como @RestController, @GetMapping, @PostMapping, etc.)
+
+import com.example.server.api.cliente.ClienteService;
+import com.example.server.database.entity.Cliente;
+import com.example.server.database.entity.HospedesAtivos;
+
+@RestController // Define a classe como controller
+@RequestMapping("/hospede/") // Definindo o caminho da API 
+public class HospedeController {
+    
+    @Autowired // O Spring vai injetar o UsuarioServico aqui para 
+    private HospedeService hospedeService;
+
+    //Get all
+    @GetMapping("/")
+    public ResponseEntity<List<HospedesAtivos>> tdsHospede() throws InterruptedException, ExecutionException {
+        CompletableFuture<List<HospedesAtivos>> tdsHopedes = hospedeService.tdsHospede();
+        List<HospedesAtivos> listHopedes = tdsHopedes.get(); 
+        return ResponseEntity.ok(listHopedes);
+        
+    }
+
+}
---
+++ server/src/main/java/com/example/server/api/hospede/HospedeService.java
@@ -0,0 +1,49 @@
+package com.example.server.api.hospede;
+
+import java.util.List; // Importa a classe List para trabalhar com listas de objetos
+import java.util.Optional; // Importa a classe Optional para tratar valores que podem ou não existir (como um valor nulo)
+import java.util.concurrent.CompletableFuture; // Importa a classe CompletableFuture para trabalhar com funções assíncronas
+
+import org.springframework.beans.factory.annotation.Autowired; // Importa o Autowired para permitir a injeção de dependências no Spring
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async; // Importa a anotação Async para marcar métodos que devem ser executados de forma assíncrona
+import org.springframework.stereotype.Service; // Importa a anotação Service para registrar a classe como um componente de serviço no Spring
+
+import com.example.server.database.entity.Cliente;
+import com.example.server.database.entity.HospedesAtivos; // Importa a entidade Cliente do pacote de entidades do banco de dados
+import com.example.server.database.repository.HospedesAtivosRepository; // Importa o repositório de Cliente para acessar dados no banco de dados
+
+@Service
+public class HospedeService {
+
+    private final HospedesAtivosRepository hospedesAtivosRepository;
+
+    @Autowired
+    public HospedeService(HospedesAtivosRepository hospedesAtivosRepository) {
+        this.hospedesAtivosRepository = hospedesAtivosRepository;
+    }
+
+    // METODOS
+
+    // ALL HOSPEDES
+    @Async
+    public CompletableFuture<List<HospedesAtivos>> tdsHospede() {
+        List<HospedesAtivos> hospedes = hospedesAtivosRepository.findAll();
+        return CompletableFuture.completedFuture(hospedes);
+    }
+
+    // checkInHospede
+    @Async
+    public CompletableFuture<HospedesAtivos> checkInHospede(HospedesAtivos hospede) {
+        HospedesAtivos criar = hospedesAtivosRepository.save(hospede);
+        return CompletableFuture.completedFuture(criar);
+    }
+
+    // checkOutHospede
+    @Async
+    public CompletableFuture<HospedesAtivos> checkOutHospede(HospedesAtivos hospede) {
+        HospedesAtivos criar = hospedesAtivosRepository.save(hospede);
+        return CompletableFuture.completedFuture(criar);
+    }
+
+}
